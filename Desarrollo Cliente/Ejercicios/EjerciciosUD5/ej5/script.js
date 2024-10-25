16d0c13cade04d7e2d290ef9828f19bc19424d1b
odps <odps31@gmail.com>
2024-10-25 13:25:37 +0200

    Terminados ejercicios CSS

--- Desarrollo Cliente/Ejercicios/EjerciciosUD5/ej5/script.js
+++ Desarrollo Cliente/Ejercicios/EjerciciosUD5/ej5/script.js
@@ -1,3 +1,18 @@
 window.addEventListener("load", function () {
   const btn = document.querySelector("#btn");
+  const divs = document.querySelectorAll("div");
+  btn.addEventListener("click", function () {
+    divs[0].textContent = randomGenerator();
+    divs[1].textContent = randomGenerator();
+    divs[2].textContent = greaterOf(
+      parseInt(divs[0].textContent),
+      parseInt(divs[1].textContent)
+    );
+  });
 });
+function randomGenerator() {
+  return Math.floor(Math.random() * 100);
+}
+function greaterOf(a, b) {
+  return a > b ? a : b;
+}
