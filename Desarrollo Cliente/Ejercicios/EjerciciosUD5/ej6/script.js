5e8e0728aaf7e069148b8f9bf085d8971af78285
odps <odps31@gmail.com>
2024-10-26 22:09:10 +0200

    Terminados ejercicios JS DOM

--- Desarrollo Cliente/Ejercicios/EjerciciosUD5/ej6/script.js
+++ Desarrollo Cliente/Ejercicios/EjerciciosUD5/ej6/script.js
@@ -1,25 +1,50 @@
+const imgs = ["1.jpg", "2.jpg", "3.jpg", "4.jpg", "5.jpg"];
+
 window.addEventListener("load", function () {
   const btn = document.querySelector("#btn");
-  const imgs = ["img1", "img2", "img3", "img4", "img5"];
 
   btn.addEventListener("click", function () {
     let filas = document.querySelector("#filas").value;
     let columnas = document.querySelector("#columnas").value;
 
+    //Se elimina la tabla y boton anterior si existe.
     let tabla = document.querySelector("table");
     if (tabla != null) {
-      while (tabla.hasChildNodes()) {
-        tabla.removeChild(tabla.firstChild);
-      }
+      tabla.remove();
+    }
+    let prueba = document.querySelector("#prueba");
+    if (prueba != null) {
+      prueba.remove();
     }
 
-    let tablaNueva = createTable(filas, columnas, imgs);
-    document.body.appendChild(tablaNueva);
+    //Se crea una tabla nueva y se insertan los valores del select.
+    let tablaNueva = createTable(filas, columnas);
+    tablaNueva.querySelectorAll("td").forEach((td) => {
+      td.appendChild(createSelection(imgs));
+      document.body.appendChild(tablaNueva);
+    });
+
+    
+    let pruebaBtn = createButton("Prueba", "prueba");
+    pruebaBtn.addEventListener("click", function () {
+
+      tablaNueva.querySelectorAll("td").forEach((td) => {
+        let select = td.querySelector("select");
+        let img = select.value;
+        td.removeChild(select);
+        td.appendChild(createImage("./img/"+img));
+      });
+    });
+
+
+
+
+    document.body.appendChild(pruebaBtn);
   });
 });
 
-//Creador de tablas
-function createTable(row, col, opts) {
+//Funciones auxiliares para la creacion de elementos.
+function createTable(row, col) {
   let table = document.createElement("table");
 
   for (let i = 0; i < row; i++) {
@@ -28,15 +53,11 @@ function createTable(row, col, opts) {
       table.rows[i].insertCell();
     }
   }
-
   table.id = "table";
-  table.querySelectorAll("td").forEach((td) => {
-    td.appendChild(createSelection(opts));
-  });
-
+  table.style.margin = "auto";
   return table;
 }
-//Metodo auxiliar para la creacion de selects.
+
 function createSelection(array) {
   if (!Array.isArray(array)) {
     return false;
@@ -52,4 +73,20 @@ function createSelection(array) {
 
     return select;
   }
+}
+
+function createButton(value, id) {
+  let baseid = id != null ? id : "";
+  let btn = document.createElement("button");
+  btn.id = baseid;
+  btn.textContent = value;
+  return btn;
+}
+
+function createImage(src) {
+  let img = document.createElement("img");
+  img.width = 100;
+  img.height = 100;
+  img.src = src;
+  return img;
 }
