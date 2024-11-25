78dcce0d0bc1162065ad9d4771b70859b50501fa
odps <odps31@gmail.com>
2024-11-25 09:30:43 +0100

    Terminado PHP Acceso a BBDD mediante DAO y DTO

---
+++ Desarrollo Entorno Servidor/UNIDAD 7/AccesoBBDD/model/business/class_cuenta.php
@@ -0,0 +1,49 @@
+<?php
+
+class cuenta {
+    public $id;
+    public $codigo;
+    public $saldo;
+    public $cliente;
+
+    public function __construct($codigo, $saldo, $cliente){
+		$this->setId(null);
+		$this->setCodigo($codigo);
+    	$this->setSaldo($saldo);
+		$this->setCliente($cliente);
+	}
+
+	public function getId(){
+		return $this->id;
+	}
+
+	public function setId($value){
+		$this->id = $value;
+	}
+
+	public function getCodigo(){
+		return $this->codigo;
+	}
+
+	public function setCodigo($value){
+		$this->codigo = $value;
+	}
+
+  	public function getSaldo(){
+		return $this->saldo;
+	}
+
+	public function setSaldo($value){
+		$this->saldo = $value;
+	}
+
+	public function getCliente(){
+		return $this->cliente;
+	}
+
+	public function setCliente($value){
+		$this->cliente = $value;
+	}
+
+}
+ ?>
