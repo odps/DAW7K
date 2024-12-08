dd841618f2b0b3217f6a1c0381e22ed60500080f
odps <odps31@gmail.com>
2024-12-08 17:44:42 +0100

    Terminado 1er ejercicio laravel y animaciones proyecto vinilo

---
+++ Desarrollo Entorno Servidor/UNIDAD 9 - LARAVEL/EJ1/ud9_pt1/app/Http/Controllers/DefaultController.php
@@ -0,0 +1,9 @@
+<?php
+namespace App\Http\Controllers;
+class DefaultController extends Controller
+{
+    function home()
+    {
+        return view('default.home');
+    }
+}
---
+++ Desarrollo Entorno Servidor/UNIDAD 9 - LARAVEL/EJ1/ud9_pt1/resources/views/navbar.blade.php
@@ -0,0 +1,6 @@
+<!-- Navigation -->
+<nav>
+    <a href="{{ route('home') }}">Inicio</a>
+    &nbsp;&nbsp;&nbsp;
+    <a href="{{ route('cuenta_list') }}">Cuentas</a>
+</nav>
