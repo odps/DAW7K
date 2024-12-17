8f8792605f9b12c8aa947d4b54657b2da409fd27
odps <odps31@gmail.com>
2024-12-17 08:07:33 +0100

    Arreglado bug en ejercicio formularios JS

--- Desarrollo Cliente/Ejercicios/Formularios/FormulariosOscarPereira/ej55.js
+++ Desarrollo Cliente/Ejercicios/Formularios/FormulariosOscarPereira/ej55.js
@@ -1,216 +1,218 @@
 window.addEventListener("DOMContentLoaded", function () {
-    //Variables globales del documento
-    const formElements = document.forms[0];
-    const labels = document.getElementsByTagName("label");
-    const botonLimpiar = document.getElementById("limpiar");
-    const botonEnviar = document.forms[0][12];
-    const nombre = document.getElementsByName('nombre')[0];
-    const numeroDni = document.getElementsByName('dni')[0];
-    const letraDNI = document.getElementsByName('letradni')[0];
-    const edad = document.getElementsByName('edad')[0];
-    const telefono = document.getElementsByName('telefono')[0];
-    const email = document.getElementsByName('email')[0];
-    const repeatemail = document.getElementsByName('repeatemail')[0];
-    const provincias = document.getElementsByName('provincia')[0];
-    let localidad = document.getElementsByName('localidad')[0];
-    const inputs = document.querySelectorAll('input[type="text"]');
-    const obligatorios = [];
+  //Variables globales del documento
+  const formElements = document.forms[0];
+  const labels = document.getElementsByTagName("label");
+  const botonLimpiar = document.getElementById("limpiar");
+  const botonEnviar = document.forms[0][12];
+  const nombre = document.getElementsByName("nombre")[0];
+  const numeroDni = document.getElementsByName("dni")[0];
+  const letraDNI = document.getElementsByName("letradni")[0];
+  const edad = document.getElementsByName("edad")[0];
+  const telefono = document.getElementsByName("telefono")[0];
+  const email = document.getElementsByName("email")[0];
+  const repeatemail = document.getElementsByName("repeatemail")[0];
+  const provincias = document.getElementsByName("provincia")[0];
+  let localidad = document.getElementsByName("localidad")[0];
+  const inputs = document.querySelectorAll('input[type="text"]');
+  const obligatorios = [];
+  let validados = {};
 
-    //Anyadimos funcionalidad de limpiar al boton sin cambiar el type
-    botonLimpiar.addEventListener("click", function () {
-        for (let i = 1; i < 12; i++) {
-            formElements[i].value = "";
-        }
-    });
-    //Verificamos cuales campos son obligatorios
-    for (let i = 0; i < labels.length; i++) {
-        let texto = labels[i].innerText;
-        if (new RegExp("\\*").test(texto)) {
-            let associatedElement = labels[i].nextElementSibling;
-            obligatorios.push(associatedElement);
-        }
+  //Anyadimos funcionalidad de limpiar al boton sin cambiar el type
+  botonLimpiar.addEventListener("click", function () {
+    for (let i = 1; i < 12; i++) {
+      formElements[i].value = "";
     }
-    //Anyadimos la funcionalidad de comprobar campos obligatorios
-    obligatorios.forEach((elemento) => {
-        elemento.addEventListener('blur', function () {
-            pintaInput(elemento, !checkVacio(elemento));
-        });
-    })
-    //Verificamos todos los campos al pulsar el boton enviar
-    botonEnviar.addEventListener('click', function (evento) {
-        evento.preventDefault();
-        let dni = numeroDni.value + letraDNI.value;
-        let isValid = true;
-
-        // Validar DNI
-        if (!validaDNI(dni)) {
-            pintaInput(numeroDni, false);
-            pintaInput(letraDNI, false);
-            isValid = false;
-        } else {
-            pintaInput(numeroDni, true);
-            pintaInput(letraDNI, true);
-        }
-
-        // Validar Edad
-        if (!validaEdad(edad.value)) {
-            pintaInput(edad, false);
-            isValid = false;
-        } else {
-            pintaInput(edad, true);
-        }
-
-        // Validar Telefono
-        if (!validaTelefono(telefono.value)) {
-            pintaInput(telefono, false);
-            isValid = false;
-        } else {
-            pintaInput(telefono, true);
-        }
-
-        // Validar Email
-        if (!validaEmail(email.value) || email.value !== repeatemail.value) {
-            pintaInput(email, false);
-            pintaInput(repeatemail, false);
-            isValid = false;
-        } else {
-            pintaInput(email, true);
-            pintaInput(repeatemail, true);
-        }
-
-        if (isValid){
-            evento.target.dispatchEvent('submit');
-        }
-
+  });
+  //Verificamos cuales campos son obligatorios
+  for (let i = 0; i < labels.length; i++) {
+    let texto = labels[i].innerText;
+    if (new RegExp("\\*").test(texto)) {
+      let elemento = labels[i].nextElementSibling;
+      obligatorios.push(elemento);
+      validados[elemento.name] = false;
+    }
+  }
+  //Anyadimos la funcionalidad de comprobar campos obligatorios
+  obligatorios.forEach((elemento) => {
+    elemento.addEventListener("blur", function () {
+      pintaInput(elemento, !checkVacio(elemento));
     });
-    //Agregamos localidades a las pronvicias.
-    provincias.addEventListener('change', function (evento) {
-        const localidades = {
-            V: ['Valencia', 'Gandia', 'Torrent'],
-            CS: ['Castellón de la Plana', 'Villarreal', 'Benicarló'],
-            A: ['Alicante', 'Elche', 'Torrevieja']
-        };
+  });
+  //Verificamos todos los campos al pulsar el boton enviar
+  botonEnviar.addEventListener("click", function (evento) {
+    evento.preventDefault();
+    let dni = numeroDni.value + letraDNI.value;
+    let isValid = true;
 
-        while (localidad.options.length > 0) {
-            localidad.remove(0);
+    // Validar DNI
+    if (!validaDNI(dni)) {
+      pintaInput(numeroDni, false);
+      pintaInput(letraDNI, false);
+      isValid = false;
+    } else {
+      pintaInput(numeroDni, true);
+      pintaInput(letraDNI, true);
+    }
+
+    // Validar Edad
+    if (!validaEdad(edad.value)) {
+      pintaInput(edad, false);
+      isValid = false;
+    } else {
+      pintaInput(edad, true);
+    }
+
+    // Validar Telefono
+    if (!validaTelefono(telefono.value)) {
+      pintaInput(telefono, false);
+      isValid = false;
+    } else {
+      pintaInput(telefono, true);
+    }
+
+    // Validar Email
+    if (!validaEmail(email.value) || email.value !== repeatemail.value) {
+      pintaInput(email, false);
+      pintaInput(repeatemail, false);
+      isValid = false;
+    } else {
+      pintaInput(email, true);
+      pintaInput(repeatemail, true);
+    }
+
+    if (isValid) {
+      evento.target.dispatchEvent("submit");
+    }
+  });
+  //Agregamos localidades a las pronvicias.
+  provincias.addEventListener("change", function (evento) {
+    const localidades = {
+      V: ["Valencia", "Gandia", "Torrent"],
+      CS: ["Castellón de la Plana", "Villarreal", "Benicarló"],
+      A: ["Alicante", "Elche", "Torrevieja"],
+    };
+
+    while (localidad.options.length > 0) {
+      localidad.remove(0);
+    }
+
+    let selectedProvince = evento.target.value;
+    let localidadesOptions = localidades[selectedProvince];
+
+    localidadesOptions.forEach(function (nombre) {
+      let option = document.createElement("option");
+      option.text = nombre;
+      option.value = nombre;
+      localidad.add(option);
+      localidad.disabled = false;
+    });
+  });
+  //Pintar de verde cuando se hace focus y blanco cuando sale (si no hay error).
+  inputs.forEach((element) => {
+    element.addEventListener("focus", function (evento) {
+      element.style.borderColor = "lightgreen";
+    });
+    element.addEventListener("blur", function (evento) {
+      if (element.style.borderColor == "lightgreen")
+        element.style.borderColor = "white";
+    });
+  });
+  //El campo nombre tiene focus al cargar la pagina.
+  nombre.focus();
+  //Cada vez que se modifique el contenido se valida el elemento, si esta mal vuelve a poner focus
+  obligatorios.forEach((element) => {
+    element.addEventListener("blur", function (evento) {
+      let isEmpty = checkVacio(element);
+
+      if (!isEmpty) {
+        validados[element.name] = true;
+      } else {
+        validados[element.name] = false;
+      }
+
+      if (isEmpty) {
+        for (const [name, validated] of Object.entries(validados)) {
+          if (!validated) {
+            setTimeout(() => {
+              const el = obligatorios.find((el) => el.name === name);
+              if (el) {
+                el.focus();
+              }
+            }, 100);
+            break;
+          }
         }
-
-        let selectedProvince = evento.target.value;
-        let localidadesOptions = localidades[selectedProvince];
-
-        localidadesOptions.forEach(function (nombre) {
-            let option = document.createElement('option');
-            option.text = nombre;
-            option.value = nombre;
-            localidad.add(option);
-            localidad.disabled = false;
-        });
+      }
     });
-    //Pintar de verde cuando se hace focus y blanco cuando sale (si no hay error).
-    inputs.forEach((element)=> {
-        element.addEventListener('focus', function (evento) {
-            element.style.borderColor = 'lightgreen';
-        })
-        element.addEventListener('blur', function (evento) {
-            if (element.style.borderColor == 'lightgreen')
-                element.style.borderColor = 'white';
-        })
-    })
-    //El campo nombre tiene focus al cargar la pagina.
-    nombre.focus();
-    //Cada vez que se modifique el contenido se valida el elemento, si esta mal vuelve a poner focus
-    inputs.forEach((element) => {
-        element.addEventListener('blur', function (evento) {
-            let isValid = true;
-
-            if (element.name === 'dni') {
-                isValid = validaDNI(element.value + letraDNI.value);
-            } else if (element.name === 'letradni') {
-                isValid = validaDNI(numeroDni.value + element.value);
-            } else if (element.name === 'edad') {
-                isValid = validaEdad(element.value);
-            } else if (element.name === 'telefono') {
-                isValid = validaTelefono(element.value);
-            } else if (element.name === 'email' || element.name === 'repeatemail') {
-                isValid = validaEmail(email.value) && email.value === repeatemail.value;
-            } else {
-                isValid = !checkVacio(element);
-            }
-
-            if (!isValid) {
-                setTimeout(() => {
-                    element.focus();
-                }, 100);
-            }
-        });
-    });
-    //Los campos con valor numerico solo deben permitir numeros
-    let numericos = [numeroDni, edad, telefono];
-    numericos.forEach((campo) => {
-        campo.addEventListener('keypress', esNumero);
-    });
+  });
+  //Los campos con valor numerico solo deben permitir numeros
+  let numericos = [numeroDni, edad, telefono];
+  numericos.forEach((campo) => {
+    campo.addEventListener("keypress", esNumero);
+  });
 });
 
 //FUNCIONES PARA VALIDAR CAMPOS
 function validaDNI(dni) {
-    let validator = new RegExp("^\\d{8}[a-zA-Z]$");
-    if (!validator.test(dni)) {
-        return false;
-    }
+  let validator = new RegExp("^\\d{8}[a-zA-Z]$");
+  if (!validator.test(dni)) {
+    return false;
+  }
 
-    const letras = "TRWAGMYFPDXBNJZSQVHLCKET";
+  const letras = "TRWAGMYFPDXBNJZSQVHLCKET";
 
-    let numero = parseInt(dni);
-    let letra = dni.charAt(8).toUpperCase();
-    let letraCalculada = letras[numero % 23];
+  let numero = parseInt(dni);
+  let letra = dni.charAt(8).toUpperCase();
+  let letraCalculada = letras[numero % 23];
 
-    return letra === letraCalculada;
+  return letra === letraCalculada;
 }
 
 function validaEdad(edad) {
-    if (isNaN(edad) || edad > 99 || edad < 0) {
-        return false;
-    } else return true;
+  if (isNaN(edad) || edad > 99 || edad < 0) {
+    return false;
+  } else return true;
 }
 
 function validaTelefono(telefono) {
-    let validator = new RegExp("^\\d{9}$");
-    if (validator.test(telefono)) {
-        return true;
-    } else return false;
+  let validator = new RegExp("^\\d{9}$");
+  if (validator.test(telefono)) {
+    return true;
+  } else return false;
 }
 
 function validaEmail(email) {
-    const re = new RegExp("^[^\\s@]+@[^\\s@]+\\.[^\\s@]+$");
-    return re.test(email);
+  const re = new RegExp("^[^\\s@]+@[^\\s@]+\\.[^\\s@]+$");
+  return re.test(email);
 }
 
 //Valida si el campo esta vacio
 function checkVacio(campo) {
-    return (campo.value == '' || campo == null || campo == undefined) ? true : false;
+  return campo.value == "" || campo == null || campo == undefined
+    ? true
+    : false;
 }
 
 //Se pasa elemento a pintar y la funcion validar( esta debe tener como return un valor booleano)
 function pintaInput(elemento, result) {
-    if (result) {
-        elemento.style.borderColor = 'white';
-        if (document.querySelector('#errormsg')) {
-            elemento.parentNode.removeChild(document.querySelector('#errormsg'))
-        }
-    } else {
-        let msg = document.createElement('p');
-        msg.id = 'errormsg';
-        msg.style.color = 'red';
-        msg.innerText = '* Verificar los campos marcados en rojo';
-        elemento.style.borderColor = 'red';
-        elemento.parentNode.appendChild(msg);
-    }
+  if (result) {
+    elemento.style.borderColor = "white";
+    let errorMsgs = elemento.parentNode.querySelectorAll(".errormsg");
+    errorMsgs.forEach((msg) => msg.remove());
+  } else {
+    let msg = document.createElement("p");
+    msg.className = "errormsg";
+    msg.style.color = "red";
+    msg.innerText = `* Verificar el campo ${elemento.name}`;
+    elemento.style.borderColor = "red";
+    elemento.parentNode.appendChild(msg);
+  }
 }
 
 //Validar si un keypress es numerico
 function esNumero(evento) {
-    const charCode = evento.charCode;
-    if (charCode < 48 || charCode > 57) {
-        evento.preventDefault();
-    }
+  const charCode = evento.charCode;
+  if (charCode < 48 || charCode > 57) {
+    evento.preventDefault();
+  }
 }
