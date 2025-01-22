2458115134209fc4de3d59777fa6b23f44e6a65f
odps <odps31@gmail.com>
2025-01-22 23:16:32 +0100

    Creada pequena guia de uso de json server para apis en front

---
+++ Desarrollo Cliente/json-server/ejemplo json-server/practica.js
@@ -0,0 +1,40 @@
+const SERVER = 'http://localhost:3000';
+window.addEventListener('load', ()=>{
+    let request = new XMLHttpRequest();
+    const contenido = document.querySelector('#user');
+    request.open('GET', SERVER + '/users');
+    request.onreadystatechange = function () {
+      if (this.status == 200 && this.readyState == 4) {
+        let response = JSON.parse(this.response);
+        contenido.innerHTML=response[1].username;
+      }  
+    };
+    request.send();
+
+    document.getElementById('userForm').addEventListener('submit', function(event) {
+        event.preventDefault();
+
+        const formData = {
+            id: document.getElementById('id').value,
+            username: document.getElementById('username').value,
+            email: document.getElementById('email').value,
+            password: document.getElementById('password').value,
+            created_at: document.getElementById('created_at').value
+        };
+
+        fetch('http://localhost:3000/users', {
+            method: 'POST',
+            headers: {
+                'Content-Type': 'application/json'
+            },
+            body: JSON.stringify(formData)
+        })
+        .then(response => response.json())
+        .then(data => {
+            console.log('Success:', data);
+        })
+        .catch((error) => {
+            console.error('Error:', error);
+        });
+    });
+})
